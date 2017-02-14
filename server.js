7717548a1a752ebff18d79f6d3fbaee613d08aab
sj291189 <sj291189@gmail.com>
2017-02-14 23:19:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article 1', function (req,res){
+app.get('/article-1', function (req,res){
     res.send('Article 1 is written here');
 });
 
-app.get('/article 2', function (req,res){
+app.get('/article-2', function (req,res){
     res.send('Article 2 is written here');
 });
 
-app.get('/article 3', function (req,res){
+app.get('/article-3', function (req,res){
     res.send('Article three is written here');
 });
 
